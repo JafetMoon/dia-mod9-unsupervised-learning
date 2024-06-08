25eea50f608d9a047d188421691f987da3d31a76
Elí Jafet Velásquez Luna <120514068+JafetMoon@users.noreply.github.com>
2024-06-07 23:14:55 -0600

    Initial Attempt: Done
    1. Test preprocesamiento:
    Notebook with tests to transform images to improve DBSCAN recognition.
    
    2. Reto de agrupación.ipynb:
    Pipeline of extract/transform/trainDBSCAN/Validation
    
    Pruebas/ : Folder with tests of hierarchy algorithm.
    
    First attempt

---
+++ README.md
@@ -0,0 +1,2 @@
+#  dia-mod9-unsupervised-learning
+ Assignments for the module 9: unsupervised learning of the Artificial Inteligence Diploma held by DGTIC UNAM
---
+++ preprocesamiento.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+from scipy.signal import convolve2d
+from sklearn.preprocessing import StandardScaler
+from crea_tabla import *
+
+def ordenar_datos(images_data, original_data, norm=True):
+    '''
+    Función para importar datos y agruparlos en diccionarios
+
+
+    Parameters:
+    -----------
+    images_data : DataFrameGroupBy
+        Agrupación de tablas de características de imágenes. Cada grupo son las características de una imagen.
+    original_data : array    
+        Contenedor de las imágenes originales
+    norm : bool    
+        Determina si se normalizan los datos de las imagenes o no
+
+    Return:
+    -------
+    tuple (dict, dict)    
+        Diccionario de imágenes con 8 características, y diccionario de imágenes originales. 
+        El "key" es el número de fotograma.
+    '''
+    originals = {}
+    images = {}
+    for i, image_df in images_data:
+        k = int(i)
+        image_df.drop(['f'], axis = 1, inplace = True)
+
+        # Almacena originales
+        original_shape = original_data[k][:,:,:3].shape
+        originals[k] = image_df[['R', 'G', 'B']].to_numpy().reshape(original_shape)
+
+    
+        if norm:
+            stscaler = StandardScaler().fit(image_df)
+            np_datos_escalados = stscaler.transform(image_df)
+            images[k] = np_datos_escalados
+            
+    return images, originals
+
+
+def preprocess(original):
+    '''
+    Preprocesa una imagen única: 
+        - Aplica suavizado gaussiano de 5x5
+        - Extrae la característica de color HSV y posición i,j de cada pixel
+        - Crea el DataFrame de la librería "crea_tabla.py" con 8 columnas
+            i,j,B,G,R,H,S,V
+
+
+    Parameters:
+    -----------
+    original : array
+        Array de los canales de color de la imagen con forma (altura, ancho, 3)
+
+    Return:
+    -------
+    DataFrame    
+        DataFrame donde cada fila es un pixel y cada columna una característica.
+    '''
+
+    ##### SUAVIZADO GAUSSIANO
+    # Kernel Gaussiano 5x5
+    kernel_gauss = np.array([[1,  4,  6,  4, 1],
+                             [4, 16, 24, 16, 4],
+                             [6, 24, 36, 24, 6],
+                             [4, 16, 24, 16, 4],
+                             [1,  4,  6,  4, 1]]) / 256
+
+
+    # Aplicar la convolución 2D para cada canal
+    image_smoothed = np.zeros_like(original)
+    for i in range(3):
+        image_smoothed[..., i] = convolve2d(original[..., i], kernel_gauss, boundary='symm', mode='same')
+
+    image_smoothed = image_smoothed.astype(np.uint8)
+
+
+    ##### TRANSFORMACIÓN
+    # Expresar resultado en 6 canales (RGB|HSV)
+    image_smoothed_bgr = image_smoothed[...,[2,1,0]]
+    cuadro_hsv = cv.cvtColor(image_smoothed_bgr, cv.COLOR_BGR2HSV)
+    image_smoothed_6channel = np.dstack((image_smoothed_bgr, cuadro_hsv))
+
+
+    # Obtener expresión adecuada para el algoritmo
+    image_4d = image_smoothed_6channel[np.newaxis, ...]
+    arreglo_datos = crea_arreglo_datos(image_4d)
+
+    # Tabla con características y derivados
+    tabla_datos = pd.DataFrame(data=arreglo_datos,
+                            columns=('f','i','j','B','G','R','H','S','V'))
+    tabla_datos.drop(['f'], axis = 1, inplace = True)
+    return tabla_datos
